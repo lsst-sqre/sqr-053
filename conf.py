161ad2210ab694fcea6efa3906bbb934ff0aaec6
Angelo Fausti <afausti@lsst.org>
2021-02-12 15:10:23 -0700

    Add the jupyter_sphinx extension
    - With the jupyter_sphinx extension use the jupyter-execute directive to execute code cells and publish the cell outputs in your thechnote.
    - Exclude files from sphinx build

--- conf.py
+++ conf.py
@@ -4,16 +4,11 @@
 # see metadata.yaml in this repo to update document-specific metadata
 
 import os
-from documenteer.sphinxconfig.technoteconf import configure_technote
 
-# Ingest settings from metadata.yaml and use documenteer's configure_technote()
-# to build a Sphinx configuration that is injected into this script's global
-# namespace.
-metadata_path = os.path.join(os.path.dirname(__file__), 'metadata.yaml')
-with open(metadata_path, 'r') as f:
-    confs = configure_technote(f)
-g = globals()
-g.update(confs)
+from documenteer.conf.technote import *
+
+extensions.extend(['jupyter_sphinx'])
+exclude_patterns = ['venv', '.ipynb_checkpoints', 'README.rst']
 
 # Add intersphinx inventories as needed
 # http://www.sphinx-doc.org/en/stable/ext/intersphinx.html
